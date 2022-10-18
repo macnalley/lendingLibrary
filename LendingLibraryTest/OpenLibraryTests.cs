5e2524396913de07f87f182b2eae15c4e0424ec2
McKenzie Nalley <macnalley@gmail.com>
2022-10-18 14:15:06 -0400

    Created test outlines for OL API

---
+++ LendingLibraryTest/OpenLibraryTests.cs
@@ -0,0 +1,27 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LendingLibraryTest;
+
+[TestClass]
+public class OpenLibraryTests
+{
+    [TestMethod]
+    public void GetOpenLibraryBookTest()
+    {
+    }
+
+    [TestMethod]
+    public void GetAuthorTest()
+    {
+    }
+
+    [TestMethod]
+    public void BookMapperTest()
+    {
+    }
+
+    [TestMethod]
+    public void GetBookTest()
+    {
+    }
+}
